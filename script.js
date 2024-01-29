c360b7f6303369df1712138da949522600b8432e
Carlos Robert <149803558+dev-carl-robert@users.noreply.github.com>
2024-01-29 10:45:16 -0300

    Responsividade e Javascript

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const caixaTexto = document.getElementById('textoTranscrito');
 const botaoTranscricao = document.getElementById('transcrever');
- 
+const micActived = document.querySelectorAll('.mic');
+
 const reconhecimento = new webkitSpeechRecognition();
 
 reconhecimento.onresult = (evento) => {
     const transcricao = evento.results[0][0].transcript;
     caixaTexto.innerText = transcricao;
 };
+reconhecimento.onstart = () => {
+    micActived.forEach(mic => mic.classList.remove('bi-mic'));
+    micActived.forEach(mic => mic.classList.add('bi-mic-fill'));
+};
 
-botaoTranscricao.addEventListener('click', () => reconhecimento.start())
+reconhecimento.onend = () => {
+    micActived.forEach(mic => mic.classList.remove('bi-mic-fill'));
+    micActived.forEach(mic => mic.classList.add('bi-mic'));
+};
+
+botaoTranscricao.addEventListener('click', () => reconhecimento.start(), )
+
+
